883c99dab1fb1d54528e31940c1fba9ee3e8b721
admin-PC\admin <puchiu.dan@gmail.com>
2017-07-27 18:29:21 +0300

    EventsEntityFrmVs

---
+++ EventsEntityFrmVs/EventsEntityFrmVs/Scripts/MyJavaScript.js
@@ -0,0 +1,15 @@
+﻿$(".subscribeButton").click(
+    function () {
+    
+        if ($(this).text() == "Subscribed")
+            $(this).text("Unsubscribed")
+        else
+            $(this).text("Subscribed")
+    })
+
+$(".buttonGoTo").click(
+    function () {
+
+        window.location.href = '/Event'
+
+    })
